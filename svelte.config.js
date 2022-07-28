afc9ba7d023b571ff16fe759e4188103e2d7bf9d
Mitchell Christ <xzmaru@gmail.com>
2022-07-27 19:52:33 -0700

    add package

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
 import adapter from '@sveltejs/adapter-netlify'
 import preprocess from 'svelte-preprocess'
 
+const packages = [
+	'snippets/marquee/Marquee.svelte',
+	'snippets/pipewriter/Pipewriter.svelte',
+]
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 		outDir: 'build',
+		package: {
+			files: filepath => packages.includes(filepath)
+		}
 	},
 	preprocess: [
 		preprocess({
