9ff03c170f2d528f82ccd1785ac7ccea77e802ac
Mitchell Christ <xzmaru@gmail.com>
2023-02-14 17:18:15 -0800

    use svh, add jsconfig

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
-<main class="grid grid-rows-[1fr,auto] gap-4 min-h-screen text-center">
+<main class="grid grid-rows-[1fr,auto] gap-4 text-center">
 	<slot></slot>
 
 	<ExternalLinks/>
 </main>
+
+<style>
+	main {
+		min-height: 100svh;
+	}
+</style>
 
 <script>
 	import ExternalLinks from '@/lib/ExternalLinks.svelte'
