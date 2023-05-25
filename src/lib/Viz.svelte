8612e54150917dafd51f30cad6122255ce43ada0
Mitchell Christ <xzmaru@gmail.com>
2023-05-24 22:40:40 -0700

    add button

---
+++ src/lib/Viz.svelte
@@ -0,0 +1,46 @@
+<div class="grid place-content-center]" style:height="{height}px">
+	{#if enabled}
+		<div class="flex flex-wrap items-center justify-center gap-8" transition:fade>
+			<FrequencyViz controls={false}
+				svgClassName="mx-auto"
+				fftSizeIndex={4}
+				minDecibels={-480}
+				offset={-150}
+				{height}
+			/>
+
+			<TimeDomainViz controls={false}
+				svgClassName="mx-auto"
+				fftSizeIndex={4}
+				{height}
+			/>
+		</div>
+
+	{:else}
+		<div class="grid place-content-center" transition:fade>
+			<button
+				class="action"
+				on:click={() => enabled = true}
+			>
+				Get lit <span class="bg-accent bg-clip-text text-transparent">🔥</span>
+			</button>
+		</div>
+	{/if}
+</div>
+
+<style>
+	.grid > :global(*) {
+		grid-column: 1 / -1;
+		grid-row: 1 / -1;
+	}
+</style>
+
+<script lang="ts">
+	import { fade } from 'svelte/transition'
+	import FrequencyViz from '$lib/audio/FrequencyViz.svelte'
+	import TimeDomainViz from '$lib/audio/TimeDomainViz.svelte'
+
+	let enabled = false
+
+	const height = 100
+</script>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,4 @@
-<div class="flex flex-wrap items-center justify-center gap-8">
-	<FrequencyViz controls={false}
-		svgClassName="mx-auto"
-		fftSizeIndex={4}
-		minDecibels={-480}
-		offset={-150}
-		height={100}
-	/>
-
-	<TimeDomainViz controls={false}
-		svgClassName="mx-auto"
-		fftSizeIndex={4}
-		height={100}
-	/>
-</div>
+<Viz/>
 
 <div class="grid">
 	<Section title="Skills">
@@ -38,16 +24,11 @@
 
 	<Section title="Motivation" className="grid gap-6 text-center">
 		<Quote>A day without failure is a day without learning.</Quote>
-
-		<Quote author="Rich Harris" cite="https://youtu.be/uXCipjbcQfM?t=270">
-			Lighthouse is a <em>diagnostic tool</em>, not a <em>scorecard</em>.
-		</Quote>
 	</Section>
 </div>
 
 <script>
-	import FrequencyViz from '$lib/audio/FrequencyViz.svelte'
-	import TimeDomainViz from '$lib/audio/TimeDomainViz.svelte'
+	import Viz from '$lib/Viz.svelte'
 	import Section from '$lib/Section.svelte'
 	import Quote from '$lib/Quote.svelte'
 
