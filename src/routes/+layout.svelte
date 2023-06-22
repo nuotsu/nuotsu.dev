ba0490ee2e837f671a735d5d8b9290967b49eeac
Mitchell Christ <xzmaru@gmail.com>
2023-06-22 15:13:56 -0700

    new

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,5 @@
-<svelte:head>
-	<title>Mitchell Christ — Web Dev</title>
-	<meta
-		name="description"
-		content="A fullstack web developer with focus on building scalable websites using modern technologies including Next.js, SvelteKit, TypeScript and Tailwind CSS."
-	>
-</svelte:head>
+<slot></slot>
 
-<header class="sticky pb-12 top-0 z-10 section bg-black/60 backdrop-blur-sm backdrop-brightness-125 text-center">
-	<h1>Mitchell Kazumaru Christ <span class="OR">||</span> nuotsu</h1>
-</header>
-
-<main>
-	<slot></slot>
-</main>
-
-<style>
-	header {
-		mask-image: linear-gradient(
-			to top,
-			transparent,
-			black 3rem,
-			black
-		);
-	}
-</style>
-
-<script lang="ts">
+<script>
 	import '../styles/app.css'
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,13 @@ export default {
 	content: ['./src/**/*.{html,svelte}'],
 	theme: {
 		extend: {
-			colors: {
-				accent: 'turquoise',
-			},
-			fontFamily: {
-				sans: `"Space Grotesk", sans-serif`,
-			},
 			screens: {
-				'xs': { min: '480px' },
-				'<xs': { max: '479px' },
-				'<sm': { max: '639px' },
-				'<md': { max: '767px' },
-				'<lg': { max: '1023px' },
-				'<xl': { max: '1279px' },
-			},
+				'<sm': { max: '640px' },
+				'<md': { max: '768px' },
+				'<lg': { max: '1024px' },
+				'<xl': { max: '1280px' },
+				'<2xl': { max: '1536px' },
+			}
 		},
 	},
 	plugins: [],
