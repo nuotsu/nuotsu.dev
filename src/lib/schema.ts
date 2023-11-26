6b26c93479774a16a0114ab4902260b153e74115
Mitchell Christ <xzmaru@gmail.com>
2023-11-26 15:31:47 -0800

    reset

--- src/lib/schema.ts
+++
@@ -1,2 +0,0 @@
-export default (json: any) =>
-	`<script type="application/ld+json">${JSON.stringify(json)}</script>`
--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<main class="grid gap-4 p-4">
-	<slot></slot>
-</main>
-
-<script>
-	import '../styles/app.css'
-</script>
