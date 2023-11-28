84b3b13c6601f6e570c261f7b3d80947105e095f
Mitchell Christ <xzmaru@gmail.com>
2023-11-27 17:42:16 -0800

    add MitsuKatsui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,58 @@
-<header>
-	<h1>nuotsu.dev</h1>
-	<h2>Mitchell Kazumaru Christ</h2>
-</header>
+<main class="grid gap-4 p-4">
+	<header>
+		<h1>nuotsu.dev</h1>
+		<h2>Mitchell Kazumaru Christ</h2>
+	</header>
 
-<section>
-	<h2>Frontend Web Development</h2>
-	<ul>
-		<li>Next.js</li>
-		<li>SvelteKit</li>
-		<li>TypeScript</li>
-		<li>Tailwind CSS</li>
-		<li>
-			Shopify
-			<a
-				href="https://www.credly.com/badges/714964a0-6bbb-4da8-9c02-e91b005fc967/linked_in_profile"
-			>
-				🎖️ <sup>Certified</sup>
-			</a>
-		</li>
-	</ul>
-</section>
+	<section>
+		<h2>Frontend Web Development</h2>
+		<ul>
+			<li>Next.js</li>
+			<li>SvelteKit</li>
+			<li>TypeScript</li>
+			<li>Tailwind CSS</li>
+			<li>
+				Shopify
+				<a
+					href="https://www.credly.com/badges/714964a0-6bbb-4da8-9c02-e91b005fc967/linked_in_profile"
+				>
+					🎖️ <sup>Certified</sup>
+				</a>
+			</li>
+		</ul>
+	</section>
 
-<section>
-	<h2>Personal Portfolio</h2>
-	<ul>
-		<li>
-			<a href="https://timeless-docs.vercel.app">🕰️ Timeless Shopify Theme</a>
-		</li>
-		<li><a href="https://blog-not.vercel.app">🤷‍♂️ The Artificial Blog</a></li>
-		<li>
-			<a
-				href="https://blog-not.vercel.app/blog/art-wall-transforming-internet-content-into-art"
-			>
-				🤳 Art Wall
-			</a>
-		</li>
-		<li><a href="https://numeration.vercel.app">🔢 Numeration</a></li>
-	</ul>
-</section>
+	<section>
+		<h2>Personal Portfolio</h2>
+		<ul>
+			<li>
+				<a href="https://timeless-docs.vercel.app">🕰️ Timeless Shopify Theme</a>
+			</li>
+			<li><a href="https://blog-not.vercel.app">🤷‍♂️ The Artificial Blog</a></li>
+			<li>
+				<a
+					href="https://blog-not.vercel.app/blog/art-wall-transforming-internet-content-into-art"
+				>
+					🤳 Art Wall
+				</a>
+			</li>
+			<li><a href="https://numeration.vercel.app">🔢 Numeration</a></li>
+			<li><a href="/mitsuo-katsui">⬛️ Mitsuo Katsui</a></li>
+		</ul>
+	</section>
 
-<section>
-	<h2>Professional Portfolio</h2>
-	<ul>
-		<li><a href="https://human.marketing">Human Marketing</a></li>
-	</ul>
-</section>
+	<section>
+		<h2>Professional Portfolio</h2>
+		<ul>
+			<li><a href="https://human.marketing">Human Marketing</a></li>
+		</ul>
+	</section>
 
-<section>
-	<h2>Contact</h2>
-	<ul>
-		<li><a href="https://github.com/nuotsu">GitHub</a></li>
-		<li><a href="https://linkedin.com/in/nuotsu">LinkedIn</a></li>
-	</ul>
-</section>
+	<section>
+		<h2>Contact</h2>
+		<ul>
+			<li><a href="https://github.com/nuotsu">GitHub</a></li>
+			<li><a href="https://linkedin.com/in/nuotsu">LinkedIn</a></li>
+		</ul>
+	</section>
+</main>
