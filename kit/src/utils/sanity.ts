e81fab889c2462b70517c64c0ca0d93ec3fe1db6
Mitchell Christ <xzmaru@gmail.com>
2024-02-05 22:40:09 -0800

    remove env vars

--- kit/src/utils/sanity.ts
+++ kit/src/utils/sanity.ts
@@ -1,17 +1,12 @@
 import { createClient } from '@sanity/client'
-import {
-	PUBLIC_SANITY_PROJECT_ID,
-	PUBLIC_SANITY_TOKEN,
-} from '$env/static/public'
 import { dev } from '$app/environment'
 
 export { default as groq } from 'groq'
 
 export const client = createClient({
-	projectId: PUBLIC_SANITY_PROJECT_ID,
+	projectId: '6puot771',
 	dataset: 'production',
 	apiVersion: '2024-01-02',
 	useCdn: !dev,
-	token: PUBLIC_SANITY_TOKEN,
 	perspective: dev ? 'previewDrafts' : 'published',
 })
