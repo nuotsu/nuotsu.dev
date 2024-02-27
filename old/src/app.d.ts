d47f84078fc0f734568c1147beada2f9c52a373e
Mitchell Christ <xzmaru@gmail.com>
2024-02-26 18:08:31 -0800

    init

--- old/src/app.d.ts
+++ old/src/app.d.ts
@@ -1,4 +1,7 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { SanityDocument } from '@sanity/client'
+
 // for information about these interfaces
 declare global {
 	namespace App {
@@ -8,6 +11,16 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+
+	namespace Sanity {
+		type Project = SanityDocument & {
+			title: string
+			emoji?: string
+			id: { current: string }
+			url: string
+			startDate: string
+		}
+	}
 }
 
-export {};
+export {}
