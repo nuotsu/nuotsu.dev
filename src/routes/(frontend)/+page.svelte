29ca6b763b67799cd49f9f1b5ba251f5a32bf7a2
Mitchell Christ <xzmaru@gmail.com>
2025-01-18 18:46:52 -0800

    rename file

--- src/routes/(frontend)/+page.svelte
+++ src/routes/(frontend)/+page.svelte
@@ -2,12 +2,12 @@
 	class="space-y-[1ch] landscape:pr-[env(safe-area-inset-right)] landscape:pl-[env(safe-area-inset-left)]"
 >
 	<ProjectList projects={data.projects} />
-	<ByYear projects={data.projects} />
+	<WebsiteCount projects={data.projects} />
 </section>
 
 <script lang="ts">
 	import ProjectList from '@/lib/ProjectList.svelte'
-	import ByYear from '@/lib/ByYear.svelte'
+	import WebsiteCount from '@/lib/WebsiteCount.svelte'
 	import type { PageData } from './$types'
 
 	let { data }: { data: PageData } = $props()
