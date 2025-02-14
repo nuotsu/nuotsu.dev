7ab3d0560745dda81606fa3a4aca9a77d7ab5e58
Mitchell Christ <xzmaru@gmail.com>
2025-02-13 23:10:04 -0800

    update mission statement

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,7 +1,7 @@
 <footer class="sticky bottom-0">
 	<!-- svelte-ignore a11y_distracting_elements -->
 	<marquee>
-		<h1>I build websites & generate images.</h1>
+		<h1>Making the world a better place, one great website at a time.</h1>
 	</marquee>
 </footer>
 
