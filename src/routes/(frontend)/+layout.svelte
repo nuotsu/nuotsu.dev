53dc6afd3e26e24569c84fc8b41a5c9049368432
Mitchell Christ <xzmaru@gmail.com>
2025-10-05 18:49:31 -0700

    unhide

--- src/routes/(frontend)/+layout.svelte
+++ src/routes/(frontend)/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Header from '@/ui/header.svelte'
 	import ProjectList from '@/ui/project-list.svelte'
-	// import PokemonTeams from '@/ui/pokemon-teams.svelte'
+	import PokemonTeams from '@/ui/pokemon-teams.svelte'
 	import CarList from '@/ui/car-list.svelte'
 	import LocalTime from '@/ui/local-time.svelte'
 	import '@/app.css'
@@ -17,7 +17,7 @@
 <main class="mx-auto flex max-w-2xl flex-col gap-lh">
 	<Header tagline={data.global?.tagline} />
 	<ProjectList projects={data.projects} />
-	<!-- <PokemonTeams teams={data.global?.pokemonTeams ?? []} /> -->
+	<PokemonTeams teams={data.global?.pokemonTeams ?? []} />
 	<CarList cars={data.global?.cars} />
 	<LocalTime />
 </main>
