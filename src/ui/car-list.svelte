dfac3639c86e8a0c0c32f60531539c8679aaca1f
Mitchell Christ <xzmaru@gmail.com>
2025-10-05 21:11:18 -0700

    add car stacking

--- src/ui/car-list.svelte
+++ src/ui/car-list.svelte
@@ -6,14 +6,19 @@
 </script>
 
 <dl
-	class="no-scrollbar flex gap-ch overflow-x-auto overflow-y-clip mask-r-from-[calc(100%-1ch)] max-md:full-bleed max-md:px-ch md:pr-ch"
+	class="no-scrollbar flex gap-lh overflow-x-auto overflow-y-clip mask-r-from-[calc(100%-1ch)] max-md:full-bleed max-md:px-ch md:pr-ch"
+	style:--gap="3.5ch"
 >
-	{#each cars as car}
-		<dt class="sticky left-0 shrink-0 bg-background text-right">
+	{#each cars as car, index}
+		<dt
+			class="sticky left-0 shrink-0 bg-background text-right before:bg-linear-to-l before:from-background before:via-background"
+		>
 			{@html car.alt?.replace(/\n/g, '<br>')}
 		</dt>
 		<dd
-			class="relative ml-[-2ch] shrink-0 bg-linear-to-l from-background to-transparent"
+			class="sticky ml-[-2ch] shrink-0 last:bg-linear-to-r last:from-transparent last:to-background"
+			style:z-index={cars.length - index}
+			style:right="calc((-1ch - var(--gap)) + ({cars.length - index} * var(--gap)))"
 		>
 			<Img
 				class="h-[2lh] w-auto"
@@ -27,16 +32,29 @@
 </dl>
 
 <style>
-	dt::after {
-		content: '';
-		position: absolute;
-		inset-block: 0;
-		inset-inline: calc(-1ch - 1px) -3ch;
-		z-index: -1;
-		background-color: var(--color-background);
+	dt {
+		&::before {
+			content: '';
+			position: absolute;
+			inset-block: 0;
+			right: 100%;
+			width: 100%;
+		}
+
+		&::after {
+			content: '';
+			position: absolute;
+			inset-block: 0;
+			inset-inline: calc(-1ch - 1px) -3ch;
+			z-index: -1;
+			background-color: var(--color-background);
+		}
 	}
 
 	dd {
-		filter: drop-shadow(-1ch 0 1ch var(--color-background));
+		filter: drop-shadow(-1ch 0 1ch var(--color-background))
+			drop-shadow(
+				1ch 0 1ch color-mix(in srgb, var(--color-background) 50%, transparent)
+			);
 	}
 </style>
