ef9867087070f2681c1870cfdac5b380c59b80c4
Mitchell Christ <xzmaru@gmail.com>
2025-12-18 01:05:28 -0800

    try process.cwd

--- src/routes/api/source-code/+server.ts
+++ src/routes/api/source-code/+server.ts
@@ -1,11 +1,14 @@
 import type { RequestHandler } from './$types'
-// @ts-ignore
 import { readFile } from 'node:fs/promises'
+import { join } from 'node:path'
 
 export const GET: RequestHandler = async ({ url }) => {
 	const file = url.searchParams.get('file')
 
-	const code = await readFile(file, 'utf-8')
+	const filePath = join(process.cwd(), file!)
+	const code = await readFile(filePath, 'utf-8')
 
-	return new Response(JSON.stringify(code))
+	return new Response(JSON.stringify(code), {
+		headers: { 'Content-Type': 'application/json' },
+	})
 }
